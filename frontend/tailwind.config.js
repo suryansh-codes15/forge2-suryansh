1d5236cf189959293ef0416d854cda26ab93b951
suryansh-codes15 <suryanshsrivastava215@gmail.com>
2026-06-27 15:04:55 +0530

    style: import compiled Tailwind CSS file to resolve layout and sidebar alignment

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,9 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.jsx",
+    "./src/*.jsx",
+    "src/**/*.jsx"
   ],
   theme: {
     extend: {
